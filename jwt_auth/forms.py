f10c0ae8a3e0b109077a68925c24b62737b24da7
shogath <7288500+shogath@users.noreply.github.com>
2022-08-13 15:32:28 +0300

    removed redundant files

--- jwt_auth/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-    email = forms.EmailField(label='Email', widget=forms.TextInput(
-        attrs={'class': 'w3-input w3-border'}))
-    password = forms.PasswordInput(label='Message', widget=forms.Textarea(
-        attrs={'class': 'w3-input w3-border'}))
