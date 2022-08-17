81562393f4099ca9e4133825075b6c26f8421d22
shogath <7288500+shogath@users.noreply.github.com>
2022-08-17 19:08:50 +0300

    removed redundant url path

--- jwt_auth/urls.py
+++ jwt_auth/urls.py
@@ -3,14 +3,12 @@ from django.urls import path
 from .views import MyObtainTokenPairView, RegisterView
 
 from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-    TokenVerifyView
+    TokenRefreshView
 )
 
 
 urlpatterns = [
     path('blog/api/login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('blog/api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('blog/api/login/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('blog/api/register/', RegisterView.as_view(), name='register_user'),
 ]
